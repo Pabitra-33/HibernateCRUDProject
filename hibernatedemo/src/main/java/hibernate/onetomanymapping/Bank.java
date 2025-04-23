d2644f43574a9a34e4697418b2fff5e66afa0980
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-24 00:01:52 +0530

    created a bank entity class

--- hibernatedemo/src/main/java/hibernate/onetomanymapping/Bank.java
+++ hibernatedemo/src/main/java/hibernate/onetomanymapping/Bank.java
@@ -1,5 +1,48 @@
 package hibernate.onetomanymapping;
 
-public class Bank {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Bank {
+	@Id
+	private int bid;
+	private String bname;
+	private String ifsc;
+	
+	@OneToMany
+	private List<Accounts> accounts;//HAS-A Relationship
+
+	//getters and setters
+	public int getBid() {
+		return bid;
+	}
+
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+
+	public String getIfsc() {
+		return ifsc;
+	}
+	public void setIfsc(String ifsc) {
+		this.ifsc = ifsc;
+	}
+
+	public List<Accounts> getAccounts() {
+		return accounts;
+	}
+	public void setAccounts(List<Accounts> accounts) {
+		this.accounts = accounts;
+	}
 }
