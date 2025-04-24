5187ac3d74de94a5c20897674ccf59377c56367d
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-24 22:06:56 +0530

    created a reviews entity class to achieve many to one relationship between two class in hibernate

---
+++ hibernatedemo/src/main/java/hibernate/manytoonemapping/Reviews.java
@@ -0,0 +1,40 @@
+package hibernate.manytoonemapping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Reviews {
+	@Id
+	private int rId;
+	private String rTitle;
+	
+	@ManyToOne
+	private Product product;//HAS-A Relationship
+
+	//getters and setters
+	public int getrId() {
+		return rId;
+	}
+
+	public void setrId(int rId) {
+		this.rId = rId;
+	}
+
+	public String getrTitle() {
+		return rTitle;
+	}
+
+	public void setrTitle(String rTitle) {
+		this.rTitle = rTitle;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
