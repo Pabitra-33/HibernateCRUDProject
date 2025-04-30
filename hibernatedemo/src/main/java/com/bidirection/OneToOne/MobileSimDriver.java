10cec0a6349d9a7970c6ac99b9f798dbe1f1fb47
Pabitra-33 <kumarpabitra345@gmail.com>
2025-04-30 23:11:33 +0530

    completed the required crud operations

--- hibernatedemo/src/main/java/com/bidirection/OneToOne/MobileSimDriver.java
+++ hibernatedemo/src/main/java/com/bidirection/OneToOne/MobileSimDriver.java
@@ -26,7 +26,7 @@ public class MobileSimDriver {
 		boolean run = true;
 		
 		while(run) {
-			System.out.println("---:Welcome to Car Management Service:---");
+			System.out.println("---:Welcome to Mobile_Sim Management Service:---");
 			System.out.println("1. Save Mobile and Sim\n2. Fetch Mobile And Sim\n3. Update Mobile\n4. Update Sim\n5. Delete Mobile And Sim\n6. Delete Sim\n7. Exit");
 			System.out.println("Enter your choice: ");
 			sc = new Scanner(System.in);
@@ -62,31 +62,123 @@ public class MobileSimDriver {
 	}
 
 	private static void saveMobileandSim() {
+		Mobile m1 = new Mobile();
+		m1.setId(4);
+		m1.setModel("IPhone 16");
+		m1.setPrice(87432);
 		
+		Sim s1 = new Sim();
+		s1.setSid(104);
+		s1.setSname("Vodafone");
+		s1.setStype("4G");
+		
+		m1.setSim(s1);//adding mobile to sim
+		s1.setMobile(m1);//adding sim to mobile
+		
+		//transactions
+		et.begin();
+		em.persist(m1);//as we are using cascade type, so when we are saving the mobile objects associated sim objects also get saved in database
+		System.out.println("Data Saved...!");
+		et.commit();
 	}
 
 	private static void fetchMobileandSim() {
-		// TODO Auto-generated method stub
+		Mobile m1 = new Mobile();
+		System.out.println("Enter the mobile id, whose data you want to fetch: ");
+		m1.setId(sc.nextInt());
 		
+		Mobile m2 = em.find(Mobile.class, m1.getId());
+		if(m2 != null) {
+			System.out.println("Mobile Details: ");
+			System.out.println("---------------");
+			System.out.println("Mobile id: "+m2.getId()+"\n"+"Mobile model: "+m2.getModel()+"\n"+"Mobile price: "+m2.getPrice());
+			
+			//get the sim
+			Sim s1 = m2.getSim();
+			System.out.println("Sim Details: ");
+			System.out.println("-------------");
+			System.out.println("Sim id: "+s1.getSid()+"\n"+"Sim name: "+s1.getSname()+"\n"+"Sim type: "+s1.getStype());
+		}
 	}
 
 	private static void updateMobile() {
-		// TODO Auto-generated method stub
+		Mobile m1 = new Mobile();
+		System.out.println("Enter the mobile id, whose data you want to update: ");
+		m1.setId(sc.nextInt());
 		
+		//find the mobile
+		Mobile m2 = em.find(Mobile.class, m1.getId());
+		if(m2 != null) {
+			System.out.println("Enter the updated mobile data: ");
+			sc.nextLine();
+			System.out.println("Enter the mobile model: ");
+			m2.setModel(sc.nextLine());
+			System.out.println("Enter the mobile price: ");
+			m2.setPrice(sc.nextInt());
+			
+			//transactions
+			et.begin();
+			em.merge(m2);
+			System.out.println("Mobile data updated..!!");
+			et.commit();
+		}
 	}
 
 	private static void updateSim() {
-		// TODO Auto-generated method stub
+		Sim s1 = new Sim();
+		System.out.println("Enter the sim id, whose data you want update: ");
+		s1.setSid(sc.nextInt());
 		
+		//find the sim
+		Sim s2 = em.find(Sim.class, s1.getSid());
+		if(s2 != null) {
+			System.out.println("Enter the updated sim data: ");
+			sc.nextLine();
+			System.out.println("Sim name: ");
+			s2.setSname(sc.nextLine());
+			System.out.println("Sim type: ");
+			s2.setStype(sc.nextLine());
+			
+			//transactions
+			et.begin();
+			em.merge(s2);
+			System.out.println("Sim data updated..!");
+			et.commit();
+		}
 	}
 
 	private static void deleteMobileandSim() {
-		// TODO Auto-generated method stub
+		Mobile m1 = new Mobile();
+		System.out.println("Enter the mobile id, whose data associated with sim data you want to delete: ");
+		m1.setId(sc.nextInt());
 		
+		//find the mobile and delete
+		Mobile m2 = em.find(Mobile.class, m1.getId());
+		if(m2 != null) {
+			
+			//transactions
+			et.begin();
+			em.remove(m2);//because of cascade type mobile data associated with sim will get deleted
+			System.out.println("Mobile deleted...");
+			et.commit();
+		}
 	}
 
 	private static void deleteSim() {
-		// TODO Auto-generated method stub
+		Mobile m1 = new Mobile();
+		System.out.println("Enter the mobile id, whose sim data you want to delete: ");
+		m1.setId(sc.nextInt());
 		
+		Mobile m2 = em.find(Mobile.class, m1.getId());
+		if(m2 != null) {
+			Sim s1 = m2.getSim();
+			m2.setSim(null);//making null so that we can delete
+			
+			//transactions
+			et.begin();
+			em.remove(s1);//sim deleted
+			System.out.println("Sim deleted...!");
+			et.commit();
+		}
 	}
 }
