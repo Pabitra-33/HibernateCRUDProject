b6624ccbfe42db89fc5e7a283fc65176844fdf2a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-01 21:54:44 +0530

    completed another method to update the teacher records from the database

--- hibernatedemo/src/main/java/com/bidirection/OneToMany/TeacherChildrensDriver.java
+++ hibernatedemo/src/main/java/com/bidirection/OneToMany/TeacherChildrensDriver.java
@@ -72,6 +72,7 @@ public class TeacherChildrensDriver {
 		sc.nextLine();
 		System.out.println("Enter techer name: ");
 		t1.setTname(sc.nextLine());
+		sc.nextLine();
 		System.out.println("Enter techer department: ");
 		t1.setTdept(sc.nextLine());
 		
@@ -114,17 +115,88 @@ public class TeacherChildrensDriver {
 	}
 
 	private static void fetchTeacherandChildrens() {
+		//teacher object
+		Teacher t1 = new Teacher();
+		System.out.println("Enter the techer id, whose data along with childrens data you want to fetch: ");
+		t1.setTid(sc.nextInt());
 		
+		Teacher t2 = em.find(Teacher.class, t1.getTid());
+		if(t2 != null) {
+			System.out.println("Teacher details: ");
+			System.out.println("================");
+			System.out.println("Teacher id: "+t2.getTid()+"\n"+"Teacher name: "+t2.getTname()+"\n"+"Teacher department: "+t2.getTdept());
+			
+			//children's data
+			List<Childrens> chList = t2.getChildrens();
+			for (Childrens childrens : chList) {
+				System.out.println("Children's Details: ");
+				System.out.println("--------------");
+				System.out.println("Children id: "+childrens.getCid()+"\n"+"Children name: "+childrens.getCname()+"\n"+"Children age: "+childrens.getCage());
+//				System.out.println("--------------");
+			}
+		}
 	}
 
 	private static void updateTeacher() {
-		// TODO Auto-generated method stub
+		//teacher object
+		Teacher t1 = new Teacher();
+		System.out.println("Enter the teacher id, whose data you want to update: ");
+		t1.setTid(sc.nextInt());
 		
+		//find the teacher
+		Teacher t2 = em.find(Teacher.class, t1.getTid());
+		if(t2 != null) {
+			System.out.println("Enter the updated techer details: ");
+			sc.nextLine();
+			System.out.println("Enter the techer name: ");
+			t1.setTname(sc.nextLine());
+			System.out.println("Enter the techer department: ");
+			t1.setTdept(sc.nextLine());
+			
+			//transactions
+			et.begin();
+			em.merge(t1);
+			System.out.println("Teacher data updated...");
+			et.commit();
+		}
 	}
 
 	private static void updateChildren() {
-		// TODO Auto-generated method stub
+		//teacher object
+		Teacher t1 = new Teacher();
+		System.out.println("Enter the teacher id, whose data you want to update: ");
+		t1.setTid(sc.nextInt());
 		
+		//find the teacher
+		Teacher t2 = em.find(Teacher.class, t1.getTid());
+		
+		if(t2 != null) {
+			//get the children details as a list
+			List<Childrens> childs = t2.getChildrens();
+			
+			//check whether the list contains data or null
+			if(childs != null) {
+				//iterate and find the child
+				for (Childrens childrens : childs) {
+					//if id matches with the user given id
+					if(childrens.getCid() == 301) {
+						System.out.println("Enter the updated children details: ");
+						sc.nextLine();
+						System.out.println("Enter the children name: ");
+						childrens.setCname(sc.nextLine());
+						System.out.println("Enter the children age: ");
+						childrens.setCage(sc.nextInt());
+//						break;
+					}
+				}
+			}
+			
+			//transaction
+			et.begin();
+			em.merge(t2);
+			System.out.println("Children's data updated..!");
+			et.commit();
+		}
 	}
 
 	private static void deleteTeacherandChildrens() {
