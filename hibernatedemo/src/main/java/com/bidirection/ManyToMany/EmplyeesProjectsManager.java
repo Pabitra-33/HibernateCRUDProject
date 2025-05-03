8f1d5b7cc5d5815ac10f691cfaab0b53a145aae5
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-03 10:19:21 +0530

    performed three pre-requisite steps to perform database connection and crud operations

---
+++ hibernatedemo/src/main/java/com/bidirection/ManyToMany/EmplyeesProjectsManager.java
@@ -0,0 +1,22 @@
+package com.bidirection.ManyToMany;
+
+import java.util.Scanner;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+//driver implementation class
+public class EmplyeesProjectsManager {
+	private static EntityManagerFactory emf; //for database connection
+	private static static EntityManager em; //for CRUD operations
+	private static EntityTransaction et; //for transaction management
+	private static Scanner sc; //for user input
+	
+	static {
+		emf = Persistence.createEntityManagerFactory("girish");
+		em = emf.createEntityManager();
+		et = em.getTransaction();
+	}
+}
