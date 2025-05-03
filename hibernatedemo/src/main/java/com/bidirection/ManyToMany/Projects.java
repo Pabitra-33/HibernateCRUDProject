b0219532fa2b82f1824447d4ec1bca27f18d49e3
Pabitra-33 <kumarpabitra345@gmail.com>
2025-05-03 10:07:15 +0530

    created a projects entity class to achieve bi-direction manyTomany mapping

--- hibernatedemo/src/main/java/com/bidirection/ManyToMany/Projects.java
+++ hibernatedemo/src/main/java/com/bidirection/ManyToMany/Projects.java
@@ -1,7 +1,10 @@
 package com.bidirection.ManyToMany;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Projects {
@@ -9,4 +12,40 @@ public class Projects {
 	private int projectId;
 	private String projectName;
 	private int CompletionDays;
+	
+	@ManyToMany(mappedBy = "projects")
+	private List<Employees> employees;
+
+	//getters and setters
+	public int getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public int getCompletionDays() {
+		return CompletionDays;
+	}
+
+	public void setCompletionDays(int completionDays) {
+		CompletionDays = completionDays;
+	}
+
+	public List<Employees> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employees> employees) {
+		this.employees = employees;
+	}
 }
